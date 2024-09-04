e309a409b443ffdd31341dc736eab493922206f1
aleksandr88p <aleksandr88p@gmail.com>
2024-09-04 23:15:43 +0000

    added something

--- ai_api/api/v1/endpoints.py
+++ ai_api/api/v1/endpoints.py
@@ -87,33 +87,6 @@ async def audio_to_text(user_id: int, file: UploadFile = File(...), token: str =
         except Exception as e:
             print(f"Failed to delete file: {e}")
 
-# @router.post("/audio-to-text/")
-# async def audio_to_text(user_id: int, file: UploadFile = File(...), token: str = Security(check_bearer_token)):
-#     try:
-#         # Сохранение файла временно
-#         file_location = f"temp_audio/{file.filename}"
-#         with open(file_location, "wb") as buffer:
-#             buffer.write(await file.read())
-
-#         # Преобразование аудио в текст
-#         recognized_text = transcribe_audio(file_location)
-
-#         # Сохранение текста во временное хранилище
-#         write_temp_data(user_id, recognized_text)
-
-#         return {
-#             "filename": file.filename,
-#             "recognized_text": recognized_text,
-#             "confirmation_required": True
-#         }
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=f"Error while processing audio")
-#     finally:
-#         try:
-#             os.remove(file_location)  # Удаление временного файла после обработки
-#         except Exception as e:
-#             print('file deleted\n{}'.format(e))
-
 
 @router.get("/get-temp-text/")
 async def get_temp_text(user_id: int, token: str = Security(check_bearer_token)):
